f96687081074347a1e83161cd73c518f3f87659a
mh-jenny-nguyen <minhhuongnguyenthi.st@gmail.com>
2022-06-02 17:57:33 +0700

    Just release to the wild

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./routes/Home";
+// import { ConnectedRouter } from "connected-react-router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/routes/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./style.module.scss";
+
+const Home = () => {
+    return <p className={style.heading}>Home</p>
+}
+
+export default Home;
